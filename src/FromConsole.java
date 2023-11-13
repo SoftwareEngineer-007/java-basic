a257f65c3e0c9cc03804f805753e55deca4c1670
Alex Selyutin <selutin.alexandr90@gmail.com>
2023-11-14 00:30:44 +0300

    string input

--- src/FromConsole.java
+++ src/FromConsole.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class FromConsole {
     public static void main(String[] args) {
+        System.out.print("Input your name: ");
+        var name = new Scanner(System.in).nextLine();
         System.out.print("Input x: ");
         var x = new Scanner(System.in).nextInt();
         System.out.print("Input y: ");
         var y = new Scanner(System.in).nextInt();
+        System.out.println(name + ", look what we got");
         System.out.print("x * y = " );
         System.out.println(x * y);
     }
