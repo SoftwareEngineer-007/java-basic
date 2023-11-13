f02214d7b772bc95c3b9ce020e01fcd5f855dc6d
Alex Selyutin <selutin.alexandr90@gmail.com>
2023-11-13 23:32:14 +0300

    multiplication and display

--- src/FromConsole.java
+++ src/FromConsole.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 public class FromConsole {
     public static void main(String[] args) {
+        System.out.print("Input x: ");
         var x = new Scanner(System.in).nextInt();
+        System.out.print("Input y: ");
         var y = new Scanner(System.in).nextInt();
+        System.out.print("x * y = " );
         System.out.println(x * y);
     }
 }
